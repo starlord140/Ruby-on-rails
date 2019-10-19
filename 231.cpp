58e8c67cc8dab1d5a03e2541f54d61e548b60840
starlord140 <56764266+starlord140@users.noreply.github.com>
2019-10-19 21:32:57 +0530

    Add files via upload

---
+++ 231.cpp
@@ -0,0 +1,23 @@
+#define rep(i,a,b) for(i=a;i<b;i++)
+#define rep1(i,a,b) for(i=a;i>b;i--)
+
+#include<bits/stdc++.h>
+
+using namespace std;
+ main()
+{
+    int i,j,k,l,m=0,a[1000][3];
+    cin>>l;
+    for(i=0;i<l;i++)
+    {k=0;
+        for(j=0;j<3;j++)
+        {cin>>a[i][j];
+        if(a[i][j]==1)
+            k++;
+    }if(k>=2)
+    {
+        m++;
+    }
+}
+cout<<m;
+}
